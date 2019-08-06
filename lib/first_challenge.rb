88bdbf236e0cbc6d9454b1268d516667b6877751
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 19:42:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {
      :montague => {"Patriarch", "Matriarch", "Hero", "Hero's Friends"},
-     :capulet => {"Patriarch", "Matriarch", "Hero", "Hero's Friends"}
+     :capulet => {"Patriarch", "Matriarch", "Heroine", "Heroine's Friends"}
    }
 end
